de9d3b2082468ed5ce1c60b288dacb54cec8dd1b
cayce2 <36436206+cayce2@users.noreply.github.com>
2024-01-23 01:16:52 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -28,3 +28,7 @@ echo "Your mail has been sent successfuly ! Thank you for your feedback";
 }
 }
 ?>
+I am writing to express my interest in the online moderation position as advertised. I am confident in my ability to contribute effectively to your team and ensure a positive online experience for users.I propose an hourly rate of $7-$10.I believe this reflects my skills and experience in the field and is competitive within the industry.I am pleased to provide references upon request. My previous employers and colleagues can attest to my dedication, reliability, and proficiency in handling online moderation tasks. Please let me know if you would like to receive a list of references to support my application.Regarding shift preferences, I am flexible and open to discussion but my preference is (time ) 
+I am excited about the opportunity to contribute to your company's success and ensure a safe and enjoyable online environment for users.
+
+https://www.onlinemoderation.com/moderationjobs/?ref=nodesk
